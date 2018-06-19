3060939695052f8f0dfcef49dee62870f5a7a9ab
huskyhoochu <dfg1499@gmail.com>
2018-06-19 20:04:59 +0900

    is_active 항목 추가 - 이메일 인증 시 is_active 활성화 계획

--- model/user.go
+++ model/user.go
@@ -8,5 +8,6 @@ type (
 		Email    string        `json:"email" bson:"email,omitempty"`
 		Password string        `json:"password" bson:"password,omitempty"`
 		Token    string        `json:"token,omitempty" bson:"-"`
+		IsActive bool          `json:"is_active" bson:"is_active"`
 	}
 )
