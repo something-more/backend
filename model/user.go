3f494cc90d268fa1a1d63b97e7e31cfacd08f92d
huskyhoochu <dfg1499@gmail.com>
2018-06-23 15:44:45 +0900

    필진 및 관리자 구분 필드 추가

--- model/user.go
+++ model/user.go
@@ -9,5 +9,7 @@ type (
 		Password string        `json:"password" bson:"password,omitempty"`
 		Token    string        `json:"token,omitempty" bson:"-"`
 		IsActive bool          `json:"is_active" bson:"is_active"`
+		IsAdmin  bool          `json:"is_admin" bson:"is_admin"`
+		IsStaff  bool          `json:"is_staff" bson:"is_staff"`
 	}
 )
