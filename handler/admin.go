52db1a80d27d9fafe220a8b06915193f60f382d2
huskyhoochu <dfg1499@gmail.com>
2018-06-27 19:48:45 +0900

    어드민 설정 핸들러 모듈화, 유저 리스트 메서드 생성

---
+++ handler/admin.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	// Default package
+	"net/http"
+	// Third Party package
+	"github.com/labstack/echo"
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	// User package
+	"github.com/backend/model"
+)
+
+func (h *Handler) ListUsers(c echo.Context) (err error) {
+
+	// Validate user
+	u := new(model.User)
+
+	userID := userIDFromToken(c)
+
+	db := h.DB.Clone()
+	defer db.Close()
+	if err = db.DB("st_more").C("users").
+		FindId(bson.ObjectIdHex(userID)).
+		One(u); err != nil {
+		if err == mgo.ErrNotFound {
+			return &echo.HTTPError{
+				Code:    http.StatusBadRequest,
+				Message: "존재하지 않는 계정입니다",
+			}
+		}
+		return
+	}
+
+	// Validate admin
+	if isAdminFromToken(c) == false {
+		return &echo.HTTPError{
+			Code: http.StatusUnauthorized,
+			Message: "이 계정은 관리자가 아닙니다",
+		}
+	}
+
+	// Find users
+	var users []*model.User
+	if err = db.DB("st_more").C("users").
+		Find(nil).
+		All(&users); err != nil {
+		return
+	}
+
+	return c.JSON(http.StatusOK, users)
+}
