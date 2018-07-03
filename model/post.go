9fbd48473522ec1c8dc677f12a092a60414f25b1
huskyhoochu <dfg1499@gmail.com>
2018-07-03 20:27:12 +0900

    post에 썸네일 필드 생성

--- model/post.go
+++ model/post.go
@@ -6,6 +6,7 @@ type (
 	Post struct {
 		ID           bson.ObjectId `json:"id" bson:"_id,omitempty"`
 		Author       string        `json:"author" bson:"author"`
+		Thumbnail    string        `json:"thumbnail" bson:"thumbnail"`
 		DateCreated  string        `json:"date_created" bson:"date_created"`
 		DateModified string        `json:"date_modified" bson:"date_modified"`
 		Title        string        `json:"title" bson:"title"`
