1e35a75ffd4b1997207ab21a56270dafdf8c83d5
huskyhoochu <dfg1499@gmail.com>
2018-07-04 19:41:49 +0900

    스토리 GET 메서드와 PATCH 메서드 충돌 회피

--- server.go
+++ server.go
@@ -66,7 +66,7 @@ func main() {
 				c.Path() == "/sign-in/" ||
 				c.Path() == "/activate/" ||
 				c.Path() == "/story/client/" ||
-				c.Path() == "/story/:story_id" ||
+				c.Path() == "/story/view/:story_id" ||
 				c.Path() == "/board/list/" ||
 				c.Path() == "/board/count/" ||
 				c.Path() == "/board/view/:board_id" ||
@@ -151,7 +151,7 @@ func main() {
 	e.GET("/story/", h.ListStory)                // 스토리 리스트
 	e.GET("/story/client/", h.ClientListStory) // 클라이언트 스토리 리스트
 	e.GET("/story/count/", h.CountStory)         // 스토리 총 갯수
-	e.GET("/story/:story_id", h.RetrieveStory)   // 스토리 디테일
+	e.GET("/story/view/:story_id", h.RetrieveStory)   // 스토리 디테일
 	e.PATCH("/story/:story_id", h.PatchStory)    // 스토리 수정
 	e.PATCH("/story/publish/:story_id", h.ChangePublishStory) // 스토리 발행 상태 변경
 	e.DELETE("/story/:story_id", h.DestroyStory) // 스토리 삭제
