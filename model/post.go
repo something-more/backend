6ffd15bccfa708e760dc222bccea0c63362e3cd2
huskyhoochu <dfg1499@gmail.com>
2018-07-05 17:58:55 +0900

    post 스키마 변경: - author 를 authorID, authorNickname으로 분할 - authorID 에는 ObjectID 값을 저장 - Nickname 은 DB 상에는 비어 있으나 값을 호출할 때 백엔드에서 매핑

--- model/post.go
+++ model/post.go
@@ -4,13 +4,14 @@ import "github.com/globalsign/mgo/bson"
 
 type (
 	Post struct {
-		ID           bson.ObjectId `json:"id" bson:"_id,omitempty"`
-		Author       string        `json:"author" bson:"author"`
-		Thumbnail    string        `json:"thumbnail" bson:"thumbnail"`
-		DateCreated  string        `json:"date_created" bson:"date_created"`
-		DateModified string        `json:"date_modified" bson:"date_modified"`
-		Title        string        `json:"title" bson:"title"`
-		Content      string        `json:"content" bson:"content"`
-		IsPublished  bool          `json:"is_published" bson:"is_published"`
+		ID             bson.ObjectId `json:"id" bson:"_id,omitempty"`
+		AuthorID       bson.ObjectId `json:"author_id" bson:"author_id"`
+		AuthorNickname string        `json:"author_nickname" bson:"author_nickname"`
+		Thumbnail      string        `json:"thumbnail" bson:"thumbnail"`
+		DateCreated    string        `json:"date_created" bson:"date_created"`
+		DateModified   string        `json:"date_modified" bson:"date_modified"`
+		Title          string        `json:"title" bson:"title"`
+		Content        string        `json:"content" bson:"content"`
+		IsPublished    bool          `json:"is_published" bson:"is_published"`
 	}
 )
